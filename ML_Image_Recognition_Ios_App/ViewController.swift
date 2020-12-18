c054c9010b3e27494f7474c11dee5417538ac051
koseali <kose.ali@std.izu.edu.tr>
2020-12-18 22:14:33 +0300

    Initial Commit

---
+++ ML_Image_Recognition_Ios_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ML_Image_Recognition_Ios_App
+//
+//  Created by Ali Köse on 18.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
