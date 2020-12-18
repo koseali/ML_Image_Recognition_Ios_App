b2f86d5928789b93eb97cef0edd752d70225f063
koseali <kose.ali@std.izu.edu.tr>
2020-12-18 22:46:20 +0300

    Created Request  for Image Recognition

--- ML_Image_Recognition_Ios_App/ViewController.swift
+++ ML_Image_Recognition_Ios_App/ViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
+import CoreML
+import Vision
+
 class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate{
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var lbExplain: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,7 +30,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
+        if let ciImage = CIImage(image: imageView.image!){
+            recognizeImage(image: ciImage)
+        }
+   
         
     }
+    
+    
+    func recognizeImage(image : CIImage)  {
+        // 1 request istek oluştur
+        // reponse oluştur Handler et
+        
+        if let model = try?  VNCoreMLModel(for: MobileNetV2().model){
+            let request = VNCoreMLRequest(model: model) { (VNRequest, error) in
+                if error != nil{
+                   // let alert = UIAlertAction(title: "Error", style: UIAlertAction.Style.default, handler: nil)
+                    
+                }
+                else{
+                    if let results = VNRequest.results as? [VNClassificationObservation]{
+                        if results.count > 0 {
+                            let topResult = results.first
+                            DispatchQueue.main.async {
+                                let confidenceLevel = topResult?.confidence ?? 0 * 100
+                                self.lbExplain.text = "\(confidenceLevel)% it is  \(topResult!.identifier)"
+                            }
+                        
+                        }
+                            
+                    }
+                }
+            
+            }
+            
+        }
+        
+       
+    }
+    
+    
+    
 }
 
